9f67de101a9fccd7a7c6ca3edbf9a0fc367f3f6f
Niklas Zimmer <niklaszimmer2902@gmail.com>
2021-11-28 17:47:58 +0100

    Create breakable pot

---
+++ Assets/Pot.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+
+    public GameObject PotFracture;
+    private Rigidbody rb;
+    public float breakVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * 10, ForceMode.Impulse); // just to debug
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter" + this.rb.velocity.magnitude);
+        var potFractureInstantiate = Instantiate(PotFracture);
+        potFractureInstantiate.transform.position = transform.position;
+        Debug.Log("ChildCount:" + PotFracture.transform.childCount);
+        
+        for(int i = 0; i < PotFracture.transform.childCount; i++)
+        {
+            var child_rb = potFractureInstantiate.transform.GetChild(i).GetComponent<Rigidbody>();
+            if (child_rb != null)
+            {
+                child_rb.velocity = rb.velocity;
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
